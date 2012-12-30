b6bb2ea2768ed192cde9b3c609be0459153e3f47
Cara Borenstein <cjb2182@columbia.edu>
2012-12-30 16:35:05 -0500

    Finish example app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ExampleApp::Application.routes.draw do
+  resources :microposts
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
